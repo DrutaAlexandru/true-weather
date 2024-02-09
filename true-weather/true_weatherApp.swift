2e6e71c8d074c49bc451d659ae163e233877f4b8
Alex Druta <drutasandu@gmail.com>
2024-02-09 10:42:46 +0200

    Created the project

---
+++ true-weather/true_weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  true_weatherApp.swift
+//  true-weather
+//
+//  Created by Alex on 09.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct true_weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
