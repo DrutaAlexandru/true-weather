d4b037dacc38a71a9f4f037baacb55014bb748d2
Alex Druta <drutasandu@gmail.com>
2024-02-10 20:34:17 +0200

    Added CitiesListViewModel

--- true-weather/Presentation/CitiesList/ViewModel/CitiesListViewModel.swift
+++ true-weather/Presentation/CitiesList/ViewModel/CitiesListViewModel.swift
@@ -6,7 +6,25 @@
 //
 
 import Foundation
+import Combine
 
 class CitiesListViewModel: ObservableObject {
     
+    var isDataDownloaded: Bool
+    
+    @Published var isLoading: Bool
+    @Published var cities: [City]
+    
+    @Published var searchText: String
+    @Published var autocompletionCities: [City]
+    
+    var showErrorPublisher = PassthroughSubject<String, Error>()
+    
+    init() {
+        isDataDownloaded = false
+        isLoading = false
+        cities = []
+        searchText = ""
+        autocompletionCities = []
+    }
 }
